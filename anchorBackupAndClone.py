86dc6b5e8422815549a6a12087faed3714f8e77e
Guido Ferreyra <guidoferreyra89@gmail.com>
2016-01-27 21:25:26 -0300

    Improved

--- anchorBackupAndClone.py
+++ anchorBackupAndClone.py
@@ -1,7 +1,7 @@
 #MenuTitle: Anchor Backup and Clone
 # -*- coding: utf-8 -*-
 __doc__="""
-(UI) Backup anchors and copy layer to layer
+(UI) Backup anchors and copy layer to layer.
 """
 
 from GlyphsApp import *
@@ -17,25 +17,28 @@ class AnchorTeleporter(object):
 	def __init__(self):
 		
 		self.createLayerList()
-
-		self.w = Window((240, 140), "Anchor Backup and Clone")
+		infoText = ""
+		self.w = Window((240, 180), "Anchor Backup and Clone")
 		self.w.button = Button((10, 10, -10, 20), "Backup Anchors",
 							callback=self.buttonCallback)
 		self.w.line = HorizontalLine((10, 40, -10, 1))
-		self.w.textBox = TextBox((10, 50, -10, 17), "Restore Anchors from")
-		self.w.popUpButton = PopUpButton((10, 80, -50, 20),
+		self.w.textBox = TextBox((10, 50, -10, 17), "Clone Anchors from")
+		self.w.popUpButton = PopUpButton((10, 75, -50, 20),
 							  self.createLayerList())
-		self.w.reloadButton = Button((-40, 80, -10, 20), u"↺",
+		self.w.reloadButton = Button((-40, 75, -10, 20), u"↺",
 							callback=self.reloadButtonCallback)
-		self.w.restoreButton = Button((10, 110, -10, 20), "Restore Anchors", callback=self.restoreCallback)
+		self.w.restoreButton = Button((10, 105, -10, 20), "Clone Anchors", callback=self.cloneCallback)
+		self.w.line2 = HorizontalLine((10, 135, -10, 1))
+		self.w.box = Box((10, 145, -10, 24))
+		self.w.box.infoBox = TextBox((0, 0, -0, -0), infoText, alignment='center', sizeStyle='small')
+
 		self.w.open()
 
 	def buttonCallback(self, sender):
 		selectedLayers = Glyphs.font.selectedLayers
-		layer = font.selectedLayers[0]
-		glyph = layer.parent
-		
+		self.w.box.infoBox.set("Anchors Backuped")
 		def createLayer (thisLayer):
+			glyph = thisLayer.parent
 			newLayer = GSLayer()
 			#print font.masters[masterId]
 			thisMaster = font.masters[thisLayer.associatedMasterId]
@@ -48,6 +51,7 @@ class AnchorTeleporter(object):
 			newLayer.name = 'AnchorBackup ' + str(relatedMaster)
 
 			return backupLayer
+
 		#Copy anchors to backup layer
 		def copyToBackup(backupLayer, anchorName, x, y):
 			newAnchor = GSAnchor.alloc().init()
@@ -68,38 +72,42 @@ class AnchorTeleporter(object):
 				# Reload List
 				layerList = self.createLayerList()
 				self.w.popUpButton.setItems( layerList )
-			print "Anchors Restored!"
 
 	def createLayerList(self):
 		selectedLayers = Glyphs.font.selectedLayers
-		layer = font.selectedLayers[0]
-		glyph = layer.parent
-
-		layerList = []
 		
-		for layer in glyph.layers:
-		    layerList.append(layer.name)
-		return layerList
+		for thisLayer in selectedLayers:
+			layerList = []
+			glyph = thisLayer.parent
+			for layer in glyph.layers:
+				layerList.append(layer.name)
+			return layerList
 
 	def reloadButtonCallback( self, sender ):
 		layerList = self.createLayerList()
 		self.w.popUpButton.setItems( layerList )
+		self.w.box.infoBox.set("Anchor list reloaded")
 
-	def restoreCallback(self, sender):
+	def cloneCallback(self, sender):
 		selectedLayers = Glyphs.font.selectedLayers
-		layer = font.selectedLayers[0]
-		glyph = layer.parent
+		self.w.box.infoBox.set("Anchors cloned!")
 		
-		def copyToFront( layer, anchorName, x, y):
+		def copyToFront( thisLayer, anchorName, x, y):
 			newAnchor = GSAnchor.alloc().init()
 			newAnchor.name = anchorName
-			layer.addAnchor_( newAnchor )
+			thisLayer.addAnchor_( newAnchor )
 			newPosition = NSPoint( x, y)
 			newAnchor.setPosition_( newPosition )
 
-		for layer in selectedLayers:
+		for thisLayer in selectedLayers:
+			glyph = thisLayer.parent
 			layerIndex = self.w.popUpButton.get()
-			backupLayer = font.glyphs[glyph.name].layers[layerIndex]
+			popchoose = self.w.popUpButton.getItems()
+		
+			for i in font.glyphs[glyph.name].layers:
+				if i.name == popchoose [layerIndex]:
+					backupLayer = i
+
 			allAnchors = backupLayer.anchors
 			
 			for thisAnchor in allAnchors:
@@ -107,9 +115,9 @@ class AnchorTeleporter(object):
 				x = thisAnchor.position.x
 				y = thisAnchor.position.y
 	
-				copyToFront (layer, anchorName, x, y)
-			print "Anchors Restored!"
+				copyToFront (thisLayer, anchorName, x, y)
+			
 
-
-
+Glyphs.clearLog()
+Glyphs.showMacroWindow()
 AnchorTeleporter()
